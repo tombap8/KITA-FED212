e0955803a3dc0054a8a96890bfea6f3909f91e95
Tom B. Parker <tombap8@hotmail.com>
2021-12-14 10:18:14 +0900

    Update main.js

--- 003.JS학습/shoppingMall_PJ/js/main.js
+++ 003.JS학습/shoppingMall_PJ/js/main.js
@@ -6,7 +6,7 @@
 window.addEventListener("DOMContentLoaded", () => {
 
     // 로딩완료확인
-    console.log("로딩완료!");
+    // console.log("로딩완료!");
 
     ////////////////////////////////
     // 배너 슬라이드 이동 구현하기 //
@@ -17,7 +17,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // 변경대상: 블릿박스 li - .indic li
     let indic = document.querySelectorAll(".indic li");
-    console.log("블릿개수:",indic.length);
+    // console.log("블릿개수:",indic.length);
 
     // 슬라이드 순번 변수
     let snum = 0;
@@ -30,7 +30,7 @@ window.addEventListener("DOMContentLoaded", () => {
     ***********************************/
     const goSlide = dir => { // dir - 방향구분
         // 1. 함수호출 확인
-        console.log("나야나!",dir);
+        // console.log("나야나!",dir);
 
         // 2. 방향에 따른 분기
         if(dir){ // 1이면 true이므로 오른쪽방향
@@ -42,12 +42,20 @@ window.addEventListener("DOMContentLoaded", () => {
             if(snum===-1) snum=4;//한계값 - 끝으로!
         } //////// else ////////
 
-        console.log("snum값:",snum);
+        // console.log("snum값:",snum);
 
         // 2. 변경대상: #slide -> slide변수에 할당!
         // 3. 변경내용: 대상의 left값을 -100% 단위로 이동함
         slide.style.left = `${-100*snum}%`;
         slide.style.transition = "left .8s ease-in-out";
+
+        // 4. 추가변경대상: .indic li -> indic변수에 할당!
+
+        // 5. 블릿li 클래스 초기화! for of문 사용!
+        for(let x of indic) x.classList.remove("on");
+
+        // 6. 변경내용: 슬라이드 순번과 같은 블릿li에 class="on"넣기
+        indic[snum].classList.add("on");
 
 
     }; //////////// goSlide 함수 ///////////
@@ -55,7 +63,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // 이벤트 대상: 이동버튼 .abtn
     let abtn = document.querySelectorAll(".abtn");
-    console.log("버튼개수:", abtn.length);
+    // console.log("버튼개수:", abtn.length);
 
     // 이동버튼 클릭설정하기 ////////////
     // 왼쪽버튼
