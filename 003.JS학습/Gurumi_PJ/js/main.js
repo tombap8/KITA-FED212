e451db039509f9315b776c6625787175ff00aaf6
Tom B. Parker <tombap8@hotmail.com>
2021-12-15 10:20:15 +0900

    Update main.js

--- 003.JS학습/Gurumi_PJ/js/main.js
+++ 003.JS학습/Gurumi_PJ/js/main.js
@@ -8,11 +8,29 @@ window.addEventListener("DOMContentLoaded", () => {
     // 갤러리박스
     let gbx = document.querySelector(".gbx");
 
+    // 광클금지변수
+    let prot = 0; // 0-허용, 1-불허용
+
     /***************************************** 
         함수명: goSlide
         기능: 슬라이드 순번을 변경하여 이동함
     *****************************************/
-    const goSlide = (dir) => { // dir - 방향(1-오른쪽,0-왼쪽)
+    const goSlide = dir => { // dir - 방향(1-오른쪽,0-왼쪽)
+
+        // 0. 광클금지 /////////////
+        if(prot) return;
+        //돌아가!(아래코드실행안함!)
+        prot = 1;//잠금!
+        setTimeout(() => {
+            prot = 0;//해제!
+        }, 400);//작동시간 0.4초후
+        ///////////////////////////
+        // 광클금지의 원리는 전역변수를 지정하고
+        // 이값에 따라 return을 주고 바로 변경하여 잠그고
+        // 일정시간뒤에 다시 처음 값으로 풀어준다!
+
+        // return은 함수를 빠져나감!
+        // 참고) break는 제어문을 빠져나감!
 
         // 1. 함수호출확인
         // console.log("바꿔라!",dir);
@@ -23,10 +41,17 @@ window.addEventListener("DOMContentLoaded", () => {
         let gimg = gbx.querySelectorAll("img");
 
         // 3. 변경내용
+
         // 3-1. 오른쪽버튼일 경우 : 맨앞 이미지 맨뒤로!
         if(dir) gbx.appendChild(gimg[0]);
+        // appendChild(처음요소) -> 처음요소 맨뒤로 이동
+
         // 3-2. 왼쪽버튼일 경우 : 맨뒤 이미지 맨앞으로!
         else gbx.insertBefore(gimg[gimg.length-1],gimg[0]);
+        // insertBefore(끝요소,처음요소) 
+        // -> 끝요소를 처음요소 앞으로 이동
+        // gimg[개수-1] -> 끝요소번호
+
 
     }; //////////// goSlide 함수 //////////////
     ///////////////////////////////////////////
