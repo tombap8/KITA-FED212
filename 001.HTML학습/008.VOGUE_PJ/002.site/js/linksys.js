aaa5dc00158d928e41c316ba0245b7a0f1f904c6
Tom B. Parker <tombap8@hotmail.com>
2021-12-15 14:14:16 +0900

    Update linksys.js

--- 001.HTML학습/008.VOGUE_PJ/002.site/js/linksys.js
+++ 001.HTML학습/008.VOGUE_PJ/002.site/js/linksys.js
@@ -5,6 +5,36 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     console.log("링크시스 로딩완료!");
 
+    /***************************************** 
+        GNB 메뉴 링크 연결하기
+        대상: .gnb a
+        변경내용: a의 텍스트를 읽어서 링크구성
+    *****************************************/
+   let gnb = document.querySelectorAll(".gnb a");
+//    console.log("메뉴개수:",gnb.length);
+
+    // GNB click이벤트 연결하기 //////
+    for(let x of gnb){
+
+        // 클릭설정하기 ////
+        x.onclick = () => {
+
+            // 1. GNB a요소 텍스트 읽기
+            let txt = x.innerText.toLowerCase().trim();
+            // toLowerCase() 는 소문자변환
+            // trim() 앞뒤공백제거
+            // 참고) toUpperCase() 는 대문자변환
+            console.log(txt);
+
+
+            // 2. 서브페이지 호출하기!
+            // Get방식으로 데이타를 넘긴다
+            // 넘길데이타: 메뉴텍스트(소문자로!)
+
+        }; /////// click //////////
+
+    } /////////// for of ///////////////
+
 
 }); /////////// 로딩구역 ////////////////////////
 /////////////////////////////////////////////////
